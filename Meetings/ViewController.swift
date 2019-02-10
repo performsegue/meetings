47ad87c213b836e29cffd3cf9533df4451081c30
bharath@incend.in <bharath@incend.in>
2019-02-10 19:27:28 +0530

    Initial Commit

---
+++ Meetings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meetings
+//
+//  Created by Bharath  Raj kumar on 10/02/19.
+//  Copyright © 2019 Bharath Raj Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
